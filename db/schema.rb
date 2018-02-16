873a88522399ebcdeee49a52ee4f05f36ed3ad1d
PRbsas <paularamirez0316@gmail.com>
2018-02-16 15:49:00 -0500

    Add name to users, modify and style devise views, modify omniauth controller and User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216160548) do
+ActiveRecord::Schema.define(version: 20180216195240) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180216160548) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
