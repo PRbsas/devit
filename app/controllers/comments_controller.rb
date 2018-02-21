b3600a9d1df06d521441a6d11ce110d39f2eb08d
PRbsas <paularamirez0316@gmail.com>
2018-02-21 16:23:13 -0500

    Create partial for error messages, clean forms, fix error in messages controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,9 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html { redirect_back(fallback_location: root_path) }
+        format.json { render json: @community.errors, status: :unprocessable_entity }
       end
     end
   end
