961413270c7cb04bd6f4b06375d85e6c2b4fcf60
PRbsas <paularamirez0316@gmail.com>
2018-03-06 18:32:00 -0500

    Add Community constructor and display list of communities

--- app/assets/javascripts/communities.js
+++ app/assets/javascripts/communities.js
@@ -1,18 +1,41 @@
-$(() => {
+window.onload = () => {
   bindClickHandlers()
-})
+}
 
 const bindClickHandlers = () => {
   $('.list_communities').on('click', (e) => {
     e.preventDefault()
+    history.pushState(null, null, 'communities')
 
     fetch(`/communities.json`, {credentials: 'same-origin'})
       .then((res) => res.json())
       .then(communities => {
         $('.wrapper').html('')
         communities.forEach((community) => {
-          console.log(community)
+          let newCommunity = new Community(community)
+          let communityHtml = newCommunity.formatIndex()
+          $('.wrapper').append(communityHtml)
         })
       })
   })
+}
+
+function Community (community) {
+  this.id = community.id
+  this.title = community.title
+  this.description = community.short_description
+}
+
+Community.prototype.formatIndex = function () {
+  let communityHtml = `
+  <section id="list">
+    <ul>
+      <li>
+        <a href="/communities/${this.id}"><h2>${this.title}</h2>
+        <p class="description">${this.description}</p>
+      </li>
+    </ul>
+  </section>
+  `
+  return communityHtml
 }
